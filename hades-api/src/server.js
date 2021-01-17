22b8aabf20fa2e45c94da9ff600af0851cf11391
Chiel Habils <chiel.habils@student.ehb.be>
2021-01-17 19:16:04 +0100

    feat(api): Added inline documentatiom

--- hades-api/src/server.js
+++ hades-api/src/server.js
@@ -20,16 +20,107 @@ http.Server(app);
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    // to support URL-encoded bodies
     extended: true
   })
 );  
 
 app.get('/test', (req, res) => {
-
   res.status(200).send();
 });
 
+/*******************************/
+/*       Boon Endpoints        */
+/*******************************/
+
+/**
+ * Get boon by UUID
+ * @param uuid 
+ * @returns  1 boon with uuid = req.params.uuid
+ */
+app.get("/boon/:uuid", async (req, res) => {
+  const result = await pg.select(['uuid', 'godname', 'content', 'created_at']).from("boonTable").where({uuid: req.params.uuid});
+  res.status(200);
+  res.json({
+    res: result,
+  });
+});
+
+/**
+ * Post a boon to the boonTable 
+ * @params godname, content 
+ * @returns  the posted boon
+ */
+app.post('/boon', async (req, res) => {
+  const uuid = Helpers.generateUUID();
+  const result = await pg
+    .insert({
+      uuid,
+      godname: req.body.godname,
+      content: req.body.content,
+      created_at: new Date(),
+    })
+    .table('boonTable')
+    .returning('*')
+    .then((res) => {
+      return res
+    })
+  res.status(200);
+  res.json({
+    res: result
+  })
+});
+
+/**
+ * Get all the boons
+ * @param 
+ * @returns  All the boons in the boonTable 
+ */
+app.get("/boons", async (req, res) => {
+  await pg.from("boonTable").select("*").then(result => {
+      res.status(200);
+      res.send(result);
+    })
+});
+
+/**
+ * Updates a specific boon
+ * @param uuid 
+ * @returns  updated boon
+ */
+app.patch("/boon/:uuid", async (req, res) => {
+  pg('boonTable')
+    .where({uuid: req.params.uuid})
+    .update(req.body)
+    .then(() => {
+      res.sendStatus(200);
+    })
+});
+
+/**
+ * Deletes a boon
+ * @param uuid 
+ * @returns nothing
+ */
+app.delete("/boon", (req, res) => {
+  let amountOfProperties = Object.keys(req.body).length;
+  if(amountOfProperties == 1 && req.body.uuid){
+    pg('boonTable').where({ uuid: req.body.uuid }).del();
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+
+/*******************************/
+/*       Gods Endpoints        */
+/*******************************/
+
+/**
+ * Get all the gods in the godTable
+ * @param 
+ * @returns  All the gods in the godTable
+ */
 app.get('/gods', async (req, res) => {
   const result = await pg
     .select('*')
@@ -40,69 +131,22 @@ app.get('/gods', async (req, res) => {
   res.status(200).send()
 })
 
-app.get('/godTable/:uuid', async (req, res) => {
-  const result = await pg.select(['uuid', 'name', 'created_at']).from('Table').where({uuid: req.params.uuid})
+/**
+ * Get a specific god by uuid
+ * @param uuid 
+ * @returns  1 god with uuid = req.params.uuid
+ */
+app.get('/god/:uuid', async (req, res) => {
+  const result = await pg.select(['uuid', 'name', 'created_at']).from('godTable').where({uuid: req.params.uuid})
   res.json({
       res: result
   })
 });
 
 
-app.get("/boonTable/:uuid", async (req, res) => {
-  const result = await pg.select(["uuid", "godname", "content", "created_at"]).from("boonTable").where({ uuid: req.params.uuid });
-  res.status(200);
-  res.json({
-    res: result,
-  });
-});
-
-
-app.post('/boonTable', async (req, res) => {
-    // let uuid = Helpers.generateUUID();
-    // pg.insert({
-    //   uuid: uuid,
-    //   //godName: req.body.godName,
-    //   content: req.body.content,
-    //   created_at: new Date(),
-    // }).into('boon').then(() => {
-    //   res.status(200);
-    //   res.json({ uuid: uuid});
-    // });
-    const uuid = Helpers.generateUUID();
-    const result = await pg
-      .insert({
-        uuid,
-        godname: req.body.godname,
-        content: req.body.content,
-        created_at: new Date(),
-      })
-      .table('boonTable')
-      .returning('*')
-      .then((res) => {
-        return res
-      })
-    res.status(200);
-    res.json({
-      res: result
-    })
-});
-
-app.get("/boonTable", async (req, res) => {
-  await pg.from("boonTable").select("*").then(result => {
-      res.status(200);
-      res.send(result);
-    })
-});
-
-app.delete("/boonTable", (req, res) => {
-  let amountOfProperties = Object.keys(req.body).length;
-  if(amountOfProperties == 1 && req.body.uuid){
-    pg('boonTable').where({ uuid: req.body.uuid }).del();
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
-  }
-});
+/*******************************/
+/*         Table inits         */
+/*******************************/
 
 async function initialiseTables() {
   await pg.schema.hasTable('godTable').then(async (exists) => {
