575f675b48d1c25c0f80875fbdc30906751833f5
Chiel Habils <chiel.habils@student.ehb.be>
2021-01-17 21:25:57 +0100

    feat(api): updated inline documentation

--- hades-api/src/server.js
+++ hades-api/src/server.js
@@ -32,6 +32,12 @@ app.get('/test', (req, res) => {
 /*       Boon Endpoints        */
 /*******************************/
 
+
+/**
+ * Post boon tto boonTable
+ * @params godname, uuid, content
+ * @returns 
+ */
 app.post("/boon", (req, res) => {
   let uuid = Helpers.generateUUID();
     pg.insert({
@@ -60,6 +66,12 @@ app.get("/boon/:uuid", async (req, res) => {
     }
 });
 
+
+/**
+ * Get all boons in boonTable
+ * @param
+ * @returns All boons
+ */
 app.get('/boons', async (req, res) => {
   const result = await pg
     .select('*')
@@ -70,6 +82,12 @@ app.get('/boons', async (req, res) => {
   res.status(200).send()
 })
 
+
+/**
+ * Updates specific boom
+ * @param uuid 
+ * @returns
+ */
 app.patch("/boon/:uuid", async (req, res) => {
     pg('boonTable')
       .where({uuid: req.params.uuid})
@@ -79,6 +97,12 @@ app.patch("/boon/:uuid", async (req, res) => {
     })
 });
 
+
+/**
+ * Deletes specific boon
+ * @param uuid 
+ * @returns
+ */
 app.delete("/boon", (req, res) => {
     pg('boonTable')
       .where({ uuid: req.body.uuid })
@@ -92,7 +116,11 @@ app.delete("/boon", (req, res) => {
 /*       Gods Endpoints        */
 /*******************************/
 
-
+/**
+ * Gets a specific god by uuid
+ * @param uuid 
+ * @returns god with uuid
+ */
 app.get('/god/:uuid', async (req, res) => {
   const result = await pg.select(['uuid', 'name', 'description', 'created_at']).from('godTable').where({uuid: req.params.uuid})
   if(result.length == 0){
@@ -102,6 +130,11 @@ app.get('/god/:uuid', async (req, res) => {
   }
 });
 
+/**
+ * Post god to godTable
+ * @params name, uuid, description
+ * @returns
+ */
 app.post('/god', async (req, res) => {
   const uuid = Helpers.generateUUID();
   const result = await pg
@@ -122,6 +155,11 @@ app.post('/god', async (req, res) => {
   })
 });
 
+/**
+ * Deletes specific god
+ * @param uuid 
+ * @returns
+ */
 app.delete("/god", (req, res) => {
     pg('godTable')
       .where({ uuid: req.body.uuid })
